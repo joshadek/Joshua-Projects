6f0a287f5cd457abbedb822c9d930524fdaf5d52
joshadek <161732473+joshadek@users.noreply.github.com>
2026-02-17 08:11:12 +0100

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,85 @@
+print(7 + 3)
+print(10 - 5)
+print(5 * 3)
+print(10 / 2)
+
+
+Name="Jacob"
+Strength=90
+
+#This is my learning file
+#print("Code Interuption")
+
+print(Name)
+ultimate_ability="Inferno storm"
+print(ultimate_ability)
+enhanced_ultimate=ultimate_ability+" level 2"
+print(enhanced_ultimate)
+#Getting player data
+print("What is your name?")
+player_name=input()
+print(player_name)
+type(player_name)
+print(type(player_name))
+a=4
+b=2
+c=4/2
+print(c)
+print(type(c))
+#change to integer and store in variable age
+age=int(input())
+
+score1 = int(input())
+score2 = int(input())
+total_score = score1+score2
+
+
+
+
+
+#lessons on control statements
+
+
+
+
+
+x=5
+if x<2:
+    print('small')
+elif x<10:
+    print('Medium')
+else:
+    print('All done')
+
+
+
+
+
+#if else statement with input and int
+age = int(input())
+if age >= 18:
+  print("Regular price")
+else:
+  print("Discount")
+print("Proceed to payment")
+
+#control statments with logic
+age = 32
+is_student = True
+if age < 18 or is_student:
+  print("Discount")
+else: 
+  print("Regular price")
+
+#false and true = false
+
+
+
+
+
+
+
+
+
+
+
